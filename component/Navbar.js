c9609dba2a2e8e3fd0f0fb690ce0ec41cdd92a58
alim <mohdalim8180036208@gmail.com>
2022-11-06 21:13:00 +0530

    contact page design done

--- component/Navbar.js
+++ component/Navbar.js
@@ -1,9 +1,19 @@
-import Link from 'next/link';
-import React from 'react'
+import Link from "next/link";
+import React,{useState} from "react";
 import styles from "../styles/Home.module.css";
 const Navbar = () => {
+  const [mode, setMode] = useState(false);
+
   return (
-    <nav className={styles.mainnav}>
+    <nav className={mode === false ? styles.mainnav : styles.mainnav1}>
+      <div>
+        <Link href="/">
+          {" "}
+          <h1 className={mode === false ? styles.logotitle : styles.logotitle1}>
+            {"<Hunting Coder/>"}
+          </h1>
+        </Link>
+      </div>
       <ul>
         <li>
           <Link href="/">Home</Link>
@@ -17,9 +27,20 @@ const Navbar = () => {
         <li>
           <Link href="/contact">Contact Us</Link>
         </li>
+        <li>
+          <button
+            type="submit"
+            className={styles.btnstyle}
+            onClick={() => {
+              mode === false ? setMode(true) : setMode(false);
+            }}
+          >
+            {mode === false ? "Light Mode" : "Dark Mode"}
+          </button>
+        </li>
       </ul>
     </nav>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
